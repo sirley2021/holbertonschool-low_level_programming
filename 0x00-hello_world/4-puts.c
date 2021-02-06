1fa461f390039e6e7397fea35fab0307f87ae359
sirley2021 <sirleylorena2@gmail.com>
2021-02-06 03:26:38 +0000

    Print using put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 
 
 /**
-*main - When we want to make it clear that a function does not have to return any value, we can do so by indicating at the beginning that the data type is going to be *"void" (null)
+*int - return value int
 *
-*Return value 0
+*decription function main - return value 0
 *
+*void: data type null
+*
+*Return: null value
 */
 int main(void)
 {
