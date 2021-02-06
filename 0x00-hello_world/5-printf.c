ee5943c6bd8b5fd969ac5a98f907b1bf720c03a7
sirley2021 <sirleylorena2@gmail.com>
2021-02-06 03:51:59 +0000

    use the function printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 
 /**
-*main - return void
+*main - return int
 *
 *return: 0 value
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
